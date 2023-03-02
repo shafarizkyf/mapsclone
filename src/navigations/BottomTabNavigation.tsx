d101cdd8919855a8bf19aecff59903f351594837
shafarizkyf <shafarizkyf@gmail.com>
2023-03-02 22:31:27 +0700

    init vector icons

--- src/navigations/BottomTabNavigation.tsx
+++ src/navigations/BottomTabNavigation.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
 import ProfileScreen from '../screens/ProfileScreen';
@@ -8,8 +9,24 @@ const Tab = createBottomTabNavigator();
 const BottomTabNavigation = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="HomeScreen" component={HomeScreen} />
-      <Tab.Screen name="ProfileScreen" component={ProfileScreen} />
+      <Tab.Screen
+        options={{
+          tabBarIcon: ({focused}) => (
+            <Icon name={focused ? 'home' : 'home-outline'} />
+          ),
+        }}
+        name="HomeScreen"
+        component={HomeScreen}
+      />
+      <Tab.Screen
+        options={{
+          tabBarIcon: ({focused}) => (
+            <Icon name={focused ? 'person' : 'person-outline'} />
+          ),
+        }}
+        name="ProfileScreen"
+        component={ProfileScreen}
+      />
     </Tab.Navigator>
   );
 };
