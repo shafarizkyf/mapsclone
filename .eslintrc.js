a8e9093c6e24beecfbcd3f596abb4f0dee3b5318
shafarizkyf <shafarizkyf@gmail.com>
2023-03-02 22:31:50 +0700

    update linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  "react/no-unstable-nested-components": [
+    "off" | "warn" | "error",
+    {
+      "allowAsProps": true,
+    }
+  ]
 };
